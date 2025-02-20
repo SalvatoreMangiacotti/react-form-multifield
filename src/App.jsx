eb8a7a60a70fbd94e3e08f8d53dc7a8726423f2b
Salvatore <workstudioms@gmail.com>
2025-02-20 14:31:53 +0100

    import fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import FruitList from './components/productsList'
+import FruitsList from './components/ProductsList'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <FruitList />
+      <FruitsList />
     </>
   )
 }
